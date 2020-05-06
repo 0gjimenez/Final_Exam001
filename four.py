4f275d353ee1360db0620f21debe06ac0f9bbe58
0gjimenez <42749723+0gjimenez@users.noreply.github.com>
2020-05-06 09:17:30 -0500

    Update four.py

--- four.py
+++ four.py
@@ -16,8 +16,44 @@
 """
 
 
-def fun():
-    pass
+def performCalculations(value):
+    result = f3(f1((f2(f2(f2(f2(value, value),value),value),value)), value), f2(f2(f1(value, f3(f2(f2(f1(value, value), f1(value, value)), f1(value, value)),value)), value), value))
+    return result
+ 
+def f1(v1, v2):
+    return v1 // v2
+
+
+def f2(v3, v4):
+    return v4 + v3
+
+
+def f3(v1, v2):
+    return v2 - v1
+
+
+def f4(v):
+    return v * v
+
+
+def f5(v1, v2):
+    return v1 / v2
+
+
+def tester(input, output):
+    actual_result = performCalculations(input)
+    expected_result = output
+    print_text = "In: " + str(input) + " | "
+    if expected_result == actual_result:
+        print_text += "Correct: " + str(expected_result) + " == " + str(actual_result)
+    else:
+        print_text += "Incorrect: " + str(expected_result) + " != " + str(actual_result)
+    return print_text
+
 
 if __name__ == "__main__":
-    fun()
+    print(tester(350, 696))
+    print(tester(37, 70))
+    print(tester(-40, -85))
+    print(tester(-2, -9))
+    print(tester(15, 26))
